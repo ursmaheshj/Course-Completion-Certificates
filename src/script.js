38c903538b48774dcf7984c715ea69d3209c96b5
Mahesh Jadhav <omkar28399@gmail.com>
2022-10-23 19:27:26 +0530

    UI changed and filtering added to certifications

--- src/script.js
+++ src/script.js
@@ -1,15 +1,41 @@
-$(document).ready(function(){
-  $(".fancybox").fancybox({
-        openEffect: "none",
-        closeEffect: "none"
-    });
+// $(document).ready(function(){
+//   $(".fancybox").fancybox({
+//         openEffect: "none",
+//         closeEffect: "none"
+//     });
     
-    $(".zoom").hover(function(){
+//     $(".zoom").hover(function(){
 		
-		$(this).addClass('transition');
-	}, function(){
+// 		$(this).addClass('transition');
+// 	}, function(){
         
-		$(this).removeClass('transition');
-	});
+// 		$(this).removeClass('transition');
+// 	});
+// });
+
+$(document).ready(function(){
+
+  $(".filter-button").click(function(){
+      var value = $(this).attr('data-filter');
+      
+      if(value == "all")
+      {
+          //$('.filter').removeClass('hidden');
+          $('.filter').show('1000');
+      }
+      else
+      {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+          $(".filter").not('.'+value).hide('3000');
+          $('.filter').filter('.'+value).show('3000');
+          
+      }
+  });
+  
+  if ($(".filter-button").removeClass("active")) {
+$(this).removeClass("active");
+}
+$(this).addClass("active");
+
 });
-    

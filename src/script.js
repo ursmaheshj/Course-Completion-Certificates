cf50a78b0f796745a813b712df32160b2654e8cd
Mahesh Jadhav <omkar28399@gmail.com>
2022-10-23 20:30:26 +0530

    filtering added

--- src/script.js
+++ src/script.js
@@ -1,17 +1,3 @@
-// $(document).ready(function(){
-//   $(".fancybox").fancybox({
-//         openEffect: "none",
-//         closeEffect: "none"
-//     });
-    
-//     $(".zoom").hover(function(){
-		
-// 		$(this).addClass('transition');
-// 	}, function(){
-        
-// 		$(this).removeClass('transition');
-// 	});
-// });
 
 $(document).ready(function(){
 
